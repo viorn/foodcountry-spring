358b03d3c8a5ab9bb85ad645816efa0478186fe1
vladimir.yuyukin <viorn89@gmail.com>
2019-12-24 23:51:48 +0300

    Was added upload file endpoint

--- src/main/java/org/taganhorn/FoodCountry/routers/DishRoute.java
+++ src/main/java/org/taganhorn/FoodCountry/routers/DishRoute.java
@@ -1,7 +1,13 @@
 package org.taganhorn.FoodCountry.routers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.taganhorn.FoodCountry.entities.response.DishResponseBody;
 import org.taganhorn.FoodCountry.entities.response.DishesListResponseBody;
 import org.taganhorn.FoodCountry.services.DishesService;
@@ -16,7 +22,8 @@ public class DishRoute {
 
     @GetMapping("list")
     Mono<DishesListResponseBody> getAllDishes(@RequestParam Integer page, @RequestParam Integer limit) {
-        if (page == null) page = 10;
+        if (page == null)
+            page = 10;
         final Integer finalPage = page;
         return Mono.fromCallable(() -> {
             return dishesService.getDishesList(finalPage, limit);
@@ -28,5 +35,22 @@ public class DishRoute {
         return Mono.fromCallable(() -> {
             return dishesService.getDishesById(id);
         }).subscribeOn(Schedulers.elastic());
+    }
+
+    static final String UPLOADED_FOLDER = "./";
+
+    @PostMapping("upload")
+    Mono<String> singleFileUpload(@RequestParam("file") MultipartFile file) {
+        return Mono.fromCallable(() -> {
+            try {
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+                Files.write(path, bytes);
+                return "ok";
+            } catch (IOException e) {
+                e.printStackTrace();
+                return e.getMessage();
+            }
+        });
     }
 }
